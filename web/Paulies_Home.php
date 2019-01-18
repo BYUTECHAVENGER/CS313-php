8f676ebbdf85e32646b46335a5986083526d430b
Paulie <sny17007@byui.edu>
2019-01-18 17:59:01 -0500

    Files created

---
+++ web/Paulies_Home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Paulie Snyder's Home Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="Paulies_Home.css" />
+    <script src="Paulies_Home.js"></script>
+</head>
+<body>
+
+
+        <div id="div1" class="Main-title">Welcome to the jungle baby!</div>
+        <div id="div2" class="Body">This is the body div.</div>
+        <div id="div3"  class="Contact-me">This is the contact me div.</div>
+
+
+
+
+    
+</body>
+</html>
