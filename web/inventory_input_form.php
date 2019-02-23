465d2d682799f252a135c88e671b6a8254c2d0b2
Paulie <sny17007@byui.edu>
2019-02-23 18:53:53 -0500

    on to ON line 37

--- web/inventory_input_form.php
+++ web/inventory_input_form.php
@@ -34,7 +34,7 @@
 </tr>
 
 <?php
-foreach ($db->query("SELECT * FROM item JOIN insurance on item.insurance_id = insurance.insurance_id JOIN owners on item.owner_id = owners.owner_id") as $row)
+foreach ($db->query("SELECT * FROM item JOIN insurance ON item.insurance_id = insurance.insurance_id JOIN owners ON item.owner_id = owners.owner_id") as $row)
 {
   echo '<tr>';
 
